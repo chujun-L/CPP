6d60305b44921c98be8f929a033c0f482dd49f3b
chujun-L <583212965@qq.com>
2019-05-29 11:12:04 +0800

    TEST8: 结构体中位域的分配、赋值及计算

--- 牛客网/main.c
+++ 牛客网/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-#define TEST7
+#define TEST8
+
+#ifdef TEST8
+struct mybitfields
+{
+	unsigned short a : 4;
+	unsigned short b : 5;
+	unsigned short c : 7;
+};
+#endif // TEST8
 
 
 int main()
@@ -21,6 +30,19 @@ int main()
 	printf("%s, %d\n", r, sizeof(long));
 
 #endif // TEST7
+
+#ifdef TEST8
+	struct mybitfields test;
+	int i = 0;
+
+	test.a = 2;
+	test.b = 3;
+	test.c = 0;
+
+	i = *((short *)&test);
+	printf("%d\n", i);
+#endif // TEST8
+
 	system("pause");
 	return 0;
 }
