8495b7f7df0d5a457be4b030932d26a0474d74d5
chujun-L <583212965@qq.com>
2019-06-09 22:55:07 +0800

    实现坦克左移动作

--- 90坦克大战/src/90TankWar.cpp
+++ 90坦克大战/src/90TankWar.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <conio.h>
+#include <graphics.h>
 #include "../include/Tank.h"
 #include "../include/map.h"
 
 int main()
 {
+	int key_board = 0;
+	int *pmap = &map[0][0];
+
 	menu();
 	captureMouse();
 
@@ -13,7 +18,34 @@ int main()
 	/* 初始化主战坦克 */
 	init_tank(TANK_POSITION_X, TANK_POSITION_Y);
 
-	system("pause");
+	while (1)
+	{
+		if (_kbhit())	// 是否有按键按下
+		{
+			key_board = _getch();
+			switch (key_board)
+			{
+			case 'a':	// 左
+				if ((my_tank.x) > 0 &&
+					*(pmap + (my_tank.y * COLS + (my_tank.x - 1))) == 0 &&
+					*(pmap + ((my_tank.y + 1) * COLS + (my_tank.x - 1))) == 0)
+				{
+					my_tank.dir = LEFT;
+					tank_walk(&my_tank, my_tank.dir, &my_tank_img[my_tank.dir]);
+				}
+				break;
+			case 'd':	// 右
+				break;
+			case 'w':	// 上
+				break;
+			case 's':	// 下
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	//system("pause");
 	closegraph();
 	return 0;
 }
