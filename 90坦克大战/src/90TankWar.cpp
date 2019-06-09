b057db20889ce56c60062d3283b842f2c63418af
chujun-L <583212965@qq.com>
2019-06-09 17:48:13 +0800

    初始化主战坦克

--- 90坦克大战/src/90TankWar.cpp
+++ 90坦克大战/src/90TankWar.cpp
@@ -6,7 +6,12 @@ int main()
 {
 	menu();
 	captureMouse();
+
+	/* 初始化地图 */
 	init_map(&map[0][0], ROWS, COLS);
+
+	/* 初始化主战坦克 */
+	init_tank(TANK_POSITION_X, TANK_POSITION_Y);
 
 	system("pause");
 	closegraph();
--- 90坦克大战/src/Tank.cpp
+++ 90坦克大战/src/Tank.cpp
@@ -96,4 +96,49 @@ void init_map(int *map, int rows, int cols)
 	}
 }
 
+/*
+ * 在地图上标记主战坦克初始化的位置
+ *
+ *  map:	地图的首地址，&map[0][0]
+ * x, y: 主战坦克初始化的位置坐标
+ * flag: 主战坦克的标记为200
+ */
+void set_prop_map(int *map, int x, int y, int flag)
+{
+	assert(map != NULL);
+	assert(flag == 200);
 
+	*(map + (y * ROWS + x)) = flag;
+	*(map + (y * ROWS + (x + 1))) = flag;
+	*(map + ((y + 1) * ROWS + x)) = flag;
+	*(map + ((y + 1) * ROWS + (x + 1))) = flag;
+}
+
+/*
+ * 初始化主战坦克
+ *
+ * x，y: 主战坦克在地图出现的坐标
+ */
+void init_tank(int x, int y)
+{
+	assert(x < COLS);
+	assert(y < ROWS);
+
+	/* 初始化主战坦克的结构体 */
+	tank_s my_tank;
+	my_tank.x = x;
+	my_tank.y = y;
+	my_tank.live = 1;
+	my_tank.dir = UP;
+
+	/* 加载主战坦克运动的4个方向的图片 */
+	IMAGE my_tank_img[4];
+	loadimage(&my_tank_img[UP], _T("Img/tank_up.jpg"), 50, 50);
+	loadimage(&my_tank_img[DOWN], _T("Img/tank_down.jpg"), 50, 50);
+	loadimage(&my_tank_img[LEFT], _T("Img/tank_left.jpg"), 50, 50);
+	loadimage(&my_tank_img[RIGHT], _T("Img/tank_right.jpg"), 50, 50);
+
+	/* 设置主战坦克的坐标并显示 */
+	set_prop_map(&map[0][0], my_tank.x, my_tank.y, 200);
+	putimage(my_tank.x * 25, my_tank.y * 25, &my_tank_img[my_tank.dir]);
+}
