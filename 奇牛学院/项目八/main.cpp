45fe71df273732dd9a9d95d86d70dc390411a795
chujun-L <583212965@qq.com>
2019-06-15 17:16:59 +0800

    项目八：双色球 练习2:用指针实现整形数组从小到大的排序

--- 奇牛学院/项目八/main.cpp
+++ 奇牛学院/项目八/main.cpp
@@ -2,11 +2,11 @@
 #include <algorithm>
 using namespace std;
 
-#define EXERCISE1
+#define EXERCISE2	
 
 int main()
 {
-#ifdef EXERCISE1
+#ifdef EXERCISE1		// 中文字符串反转
 	char s[] = "奇牛学院";
 	int len = strlen(s);
 	char tmp[256];
@@ -37,6 +37,38 @@ int main()
 	}
 	printf("%s\n", s);
 #endif // !EXERCISE1
+
+#ifdef EXERCISE2		// 用指针实现整形数组从小到大的排序
+	int a[] = {4, 2, 5, 1, 8, 3, 10, 6, 4};
+	int *p1 = NULL, *p2 = NULL;
+	int tmp = 0;
+	int len = sizeof(a) / sizeof(int);
+
+
+	for (int i = 0; i < len - 1; ++i)
+	{
+		p1 = a + i;
+		p2 = a + i + 1;
+
+		for (int j = 1; j < len - i; ++j)
+		{
+			if (*p1 > * p2) // 前一个数比后一个数大就交换
+			{
+				tmp = *p1;
+				*p1 = *p2;
+				*p2 = tmp;
+			}
+			++p2;
+		}
+	}
+
+	for (int i = 0; i < len; ++i)
+	{
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+#endif // EXERCISE2
+
 
 	return 0;
 }
